1ac5f633c6f5aeb23f9dfae3700433b3e3f959e2
George Abraham <37941352+georgeabr@users.noreply.github.com>
2025-05-13 18:42:50 +0100

    Update variables.tf

--- azure-tf-1/variables.tf
+++ azure-tf-1/variables.tf
@@ -2,3 +2,8 @@ variable "ssh_public_key" {
   type = string
   description = "SSH Public Key for VM authentication"
 }
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
